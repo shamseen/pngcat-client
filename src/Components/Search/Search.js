942741380bc38f52682790ade43fbd354ed46e91
hengyuni <hengyuni@gmail.com>
2021-05-19 11:29:06 -0400

    test test

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,68 +1,62 @@
 import { useContext, useEffect, useState } from 'react';
 import { DataContext } from '../../App';
-import SearchResults from '../SearchResults/SearchResults'
-import * as searchStyle from './Search.module.css'
-
-
+import SearchResults from '../SearchResults/SearchResults';
+import * as searchStyle from './Search.module.css';
 
 export default function Search() {
-  const { handleSubmit, searchResults } = useContext(DataContext);
+	const { handleSubmit, searchResults } = useContext(DataContext);
 
-  useEffect(async () => {
-    console.log(searchResults)
-  }, []);
+	useEffect(async () => {
+		console.log(searchResults);
+	}, []);
 
-  const handleChange = (e) => {
-    /* setSearch */
-  }
+	const handleChange = (e) => {
+		/* setSearch */
+	};
 
-  return (
-    <div className={searchStyle.searchContainer}>
-      <h2 className={searchStyle.header}>Search</h2>
+	return (
+		<div className={searchStyle.searchContainer}>
+			<h2 className={searchStyle.header}>Search</h2>
 
-      {/* ------- Search form --------- */}
-      <form onSubmit={handleSubmit} >
+			{/* ------- Search form --------- */}
+			<form onSubmit={handleSubmit}>
+				Author:{' '}
+				<input
+					className={searchStyle.input}
+					type="text"
+					id="author"
+					placeholder="Author"
+					onChange={handleChange}
+				/>
+				<br />
+				Article DOI:{' '}
+				<input
+					className={searchStyle.input}
+					type="text"
+					id="articleDOI"
+					placeholder="DOI link"
+					onChange={handleChange}
+				/>
+				<br />
+				Keyword:{' '}
+				<input
+					className={searchStyle.input}
+					type="text"
+					id="keyword"
+					placeholder="keywords"
+					onChange={handleChange}
+				/>
+				<br />
+				<input type="submit" value="Search" className={searchStyle.searchBtn} />
+			</form>
 
-        Author:{" "}
-        <input
-          className={searchStyle.input}
-          type="text"
-          id="author"
-          placeholder="Author"
-          onChange={handleChange}
-        />
-        <br />
-          Article DOI:{" "}
-        <input
-          className={searchStyle.input}
-          type="text"
-          id="articleDOI"
-          placeholder="DOI link"
-          onChange={handleChange}
-        />
-        <br />
-          Keyword:{" "}
-        <input
-          className={searchStyle.input}
-          type="text"
-          id="keyword"
-          placeholder="keywords"
-          onChange={handleChange}
-        />
-        <br />
-        <input type="submit" value="Search" className={searchStyle.searchBtn} />
-      </form>
-
-      {/* ------- Results --------- */}
-      <div>
-        <h1>Results</h1>
-        {
-          searchResults.map((res, i) => {
-            return <SearchResults result={res} key={i} />
-          })
-        }
-      </div>
-    </div>
-  )
+			{/* ------- Results --------- */}
+			<div>
+				<h1>Results</h1>
+				{searchResults.map((res, i) => {
+					return <SearchResults result={res} key={i} />;
+				})}
+			</div>
+		</div>
+	);
 }
-
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,24 +1,21 @@
-import Search from "../Search/Search"
+import Search from '../Search/Search';
 
 const SearchResults = ({ result }) => {
-
-  return (
-    <div id="result">
-      <hr />
-      {
-        Object.keys(result).map((prop, i) => {
-          return (
-            <div key={i}>
-              <h4>{prop}:</h4>
-              <span>{JSON.stringify(result[prop])}</span>
-              <br />
-              <br />
-            </div>
-          )
-        })
-      }
-    </div>
-  )
-}
+	return (
+		<div id="result">
+			<hr />
+			{Object.keys(result).map((prop, i) => {
+				return (
+					<div key={i}>
+						<h4>{prop}:</h4>
+						<span>{JSON.stringify(result[prop])}</span>
+						<br />
+						<br />
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default SearchResults;
