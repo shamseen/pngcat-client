e250c1c72f2b0ab5540a2d6d07d06e8f8acecc35
shamseen <shamseen.c@gmail.com>
2021-05-20 15:59:53 -0400

    formatted icons & alignments

--- src/Components/DragBox/DragBox.js
+++ src/Components/DragBox/DragBox.js
@@ -9,7 +9,7 @@ export default function DragBox() {
 		<div className={DragBoxStyle.DragBox}>
 			<h3 className={DragBoxStyle.header}>Drag and Drop</h3>
 			<main className={DragBoxStyle.flexbox}>
-				<Board id="board-1" className={DragBoxStyle.board}>
+				<Board id="glyph-bank" className={DragBoxStyle.board}>
 					{
 						glyphs.map((glyph, i) => {
 							return (
@@ -26,18 +26,7 @@ export default function DragBox() {
 					}
 				</Board>
 
-				<Board id="board-2" className={DragBoxStyle.board}>
-					<Card id="card-other" className={DragBoxStyle.card} draggable="true">
-						{/* <p>Glyph 2</p> */}
-						<img
-							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgflip.com%2F37i3fq.png&f=1&nofb=1"
-							alt="doomer wojack"
-							width="100"
-							height="100"
-							id="card-other"
-							className={DragBoxStyle.image}
-						/>
-					</Card>
+				<Board id="pngcat-board" className={DragBoxStyle.board}>
 				</Board>
 			</main>
 		</div>
