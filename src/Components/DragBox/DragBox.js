78f0f6e4b6e70f25655b3464ac829d48d37643b1
shamseen <shamseen.c@gmail.com>
2021-05-20 16:45:57 -0400

    save button, logs dragged glyphs

--- src/Components/DragBox/DragBox.js
+++ src/Components/DragBox/DragBox.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 import Card from "./Card";
 import { glyphs } from "./Glyphs";
 import * as DragBoxStyle from "./DragBox.module.css";
 
 export default function DragBox() {
+	const [pngcat, setPngcat] = useState([]);
+
+	const savePngcat = () => {
+		const glyphsToSave = [];
+
+		// grabbing all images dragged to the second board
+		const cardElements = document.getElementById('pngcat-board').childNodes;
+
+		// storing the glyph names as strings
+		cardElements.forEach(c => {
+			glyphsToSave.push(c.id)
+		});
+
+		setPngcat(glyphsToSave);
+	}
+
 	return (
 		<div className={DragBoxStyle.DragBox}>
 			<h3 className={DragBoxStyle.header}>Drag and Drop</h3>
 			<main className={DragBoxStyle.flexbox}>
+				<button type='button' id="save-pngcat" onClick={savePngcat}>Save!</button>
+
 				<Board id="glyph-bank" className={DragBoxStyle.board}>
 					{
 						glyphs.map((glyph, i) => {
 							return (
-								<Card key={i} id={`card-${i}`} className={DragBoxStyle.card} draggable="true">
+								<Card key={i} id={glyph} className={DragBoxStyle.card} draggable="true">
 									<img
-										id={`card-${i}`}
+										id={glyph}
 										src={`./assets/${glyph}.png`}
 										alt={`${glyph} symbol`}
 										className={DragBoxStyle.image}
