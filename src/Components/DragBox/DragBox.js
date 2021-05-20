a17735001f2dc2bb4539095dea651f358c25bb9b
shamseen <shamseen.c@gmail.com>
2021-05-20 15:42:58 -0400

    glyph pngs in repo, can drag and drop #10

--- src/Components/DragBox/DragBox.js
+++ src/Components/DragBox/DragBox.js
@@ -1,36 +1,40 @@
 import React from "react";
 import Board from "./Board";
 import Card from "./Card";
+import { glyphs } from "./Glyphs";
 import * as DragBoxStyle from "./DragBox.module.css";
 
-function DragBox() {
+export default function DragBox() {
 	return (
 		<div className={DragBoxStyle.DragBox}>
 			<h3 className={DragBoxStyle.header}>Drag and Drop</h3>
 			<main className={DragBoxStyle.flexbox}>
 				<Board id="board-1" className={DragBoxStyle.board}>
-					<Card id="card-1" className={DragBoxStyle.card} draggable="true">
-						{/* <p>Glyph 1</p> */}
-						<img
-							src="https://img.fireden.net/ic/image/1584/74/1584745629905.jpg"
-							alt="depressed wojack"
-							width="100"
-							height="100"
-							id="card-1"
-							className={DragBoxStyle.image}
-						/>
-					</Card>
+					{
+						glyphs.map((glyph, i) => {
+							return (
+								<Card key={i} id={`card-${i}`} className={DragBoxStyle.card} draggable="true">
+									<img
+										id={`card-${i}`}
+										src={`./assets/${glyph}.png`}
+										alt={`${glyph} symbol`}
+										className={DragBoxStyle.image}
+									/>
+								</Card>
+							)
+						})
+					}
 				</Board>
 
 				<Board id="board-2" className={DragBoxStyle.board}>
-					<Card id="card-2" className={DragBoxStyle.card} draggable="true">
+					<Card id="card-other" className={DragBoxStyle.card} draggable="true">
 						{/* <p>Glyph 2</p> */}
 						<img
 							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgflip.com%2F37i3fq.png&f=1&nofb=1"
 							alt="doomer wojack"
 							width="100"
 							height="100"
-							id="card-2"
+							id="card-other"
 							className={DragBoxStyle.image}
 						/>
 					</Card>
@@ -39,5 +43,3 @@ function DragBox() {
 		</div>
 	);
 }
-
-export default DragBox;
