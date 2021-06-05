9e58c5e31fe7ab15cbe5d575c350faa5706c6bf5
shamseen <shamseen.c@gmail.com>
2021-06-04 23:02:34 -0400

    navbar styled w scss #25

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import * as navStyle from "../Styles/Navbar.module.css";
+import "../Styles/navbar.scss";
 import { useContext } from 'react';
 import { DataContext } from '../App';
 
@@ -9,39 +9,38 @@ const Navbar = () => {
 
   console.log(!activeSeq.Seq_Accession);
   return (
-    <header className={navStyle.navbarHeader}>
-      <nav className={navStyle.navbarNav}>
-        <div className={navStyle.logoContainer}>
+    <header>
+      <nav>
+        <div id="logo-container">
           {/* <Link className={navStyle.logo} to="/" exact>
 						.pnGCAT
 					</Link> */}
           {/* <h2 className={navStyle.logo}>.pnGCAT</h2> */}
           <img
             src="https://d1w8c6s6gmwlek.cloudfront.net/oldshirtybastard.com/overlays/343/384/34338491.png"
-            className={navStyle.logo}
             width="30px"
           />
         </div>
-        <div className={navStyle.navItemsContainer}>
-          <Link className={navStyle.navItem} to="/" exact>
+        <div id="nav-items-container">
+          <Link className="nav-item" to="/" exact>
             Home
 					</Link>
-          <Link className={navStyle.navItem} to="/Browse">
+          <Link className="nav-item" to="/Browse">
             Browse
 					</Link>
 
           {/* user can't enter drag and drop until selecting seqence */}
           {!activeSeq.Study_Accession ? null
-            : <Link className={navStyle.navItem} to="/Box">
+            : <Link className="nav-item" to="/Box">
               Drag-n-Drop
 							</Link>
           }
 
-          <Link className={navStyle.navItem} to="/About">
+          <Link className="nav-item" to="/About">
             About
 					</Link>
         </div>
-        <div className={navStyle.activeSeq}>
+        <div id="active-seq">
           <span id='seq-id'>Sequence:&nbsp;
 						<a href={`${baseUrl}/${activeSeq.Seq_Accession}`}>
               {activeSeq.Seq_Accession}
